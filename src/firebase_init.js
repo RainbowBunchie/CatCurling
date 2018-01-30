6849833983e6fdb786ea5774dad535dcec37b161
Alexander Tischhart <atischhart.mmt-b2016@fh-salzburg.ac.at>
2018-01-30 13:55:26 +0100

    change favicon

--- src/firebase_init.js
+++ src/firebase_init.js
@@ -1,10 +1,22 @@
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
 var config = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  storageBucket: "<BUCKET>.appspot.com",
-  messagingSenderId: "<SENDER_ID>",
+apiKey: "AIzaSyB4GLvhJf7dk5OjzvILZ4NdwTRFeT9lXDQ",
+authDomain: "catcurling-f7ad1.firebaseapp.com",
+databaseURL: "https://catcurling-f7ad1.firebaseio.com",
+projectId: "catcurling-f7ad1",
+storageBucket: "catcurling-f7ad1.appspot.com",
+messagingSenderId: "611740374024"
 };
 firebase.initializeApp(config);
+
+firebase.auth().signInAnonymously().catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+
+
+
